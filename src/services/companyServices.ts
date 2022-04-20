c37b425e83a9818fc523dcbb48fee6d474fb59c0
Celso <celsolnv27@gmail.com>
2022-04-19 23:47:40 -0300

    Preparando revisão de código

--- src/services/companyServices.ts
+++
@@ -1,8 +0,0 @@
-import * as companyRepository from "../repositories/companyRepository.js";
-import * as errors from '../middlewares/handleErrorsMiddleware.js'
-
-export async function validateApiKey(apiKey: string) {
-    const company = await companyRepository.findByApiKey(apiKey);
-    if (!company) throw errors.notFound(`company api key "${apiKey}" not found`);
-    return company;
-}
